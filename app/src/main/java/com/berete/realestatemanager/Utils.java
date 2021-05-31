33b026c6e345272b628977e14a7825f1f44b49b4
Sita Bérété <sita.berete.3@gmail.com>
2021-05-30 20:03:14 -0400

    init the project

---
+++ app/src/main/java/com/berete/realestatemanager/Utils.java
@@ -0,0 +1,46 @@
+package com.berete.realestatemanager;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Philippe on 21/02/2018.
+ */
+
+public class Utils {
+
+    /**
+     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param dollars
+     * @return
+     */
+    public static int convertDollarToEuro(int dollars){
+        return (int) Math.round(dollars * 0.812);
+    }
+
+    /**
+     * Conversion de la date d'aujourd'hui en un format plus approprié
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @return
+     */
+    public static String getTodayDate(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        return dateFormat.format(new Date());
+    }
+
+    /**
+     * Vérification de la connexion réseau
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param context
+     * @return
+     */
+    public static Boolean isInternetAvailable(Context context){
+        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        return wifi.isWifiEnabled();
+    }
+}
